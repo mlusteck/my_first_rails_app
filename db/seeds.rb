96b52f5ad8aa16287efb7356b8c93ebb2eb032b5
Mischa Lusteck <mlusteck@googlemail.com>
2018-07-20 11:29:11 +0200

    4.2 Create a product in seed.rb, add product.color field (need to run db:migrate)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create( name:"seed-created Product", description:"This product was created using seed.rb.",
+                image_url: "img.jpg")
+                
