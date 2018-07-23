6ea0c544622c071c364e4d81ffc62f769080b39c
Mischa Lusteck <mlusteck@googlemail.com>
2018-07-23 16:00:56 +0200

    4.5 Add carousel, use product partial, create some product helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #test if asset exists (for displaying images)
+  def asset_exist?(path)
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include? path
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,15 @@
 module ProductsHelper
+  def product_image_tag(product)
+    if asset_exist? product.image_url
+      image_tag product.image_url, class:"img-fluid"
+    else
+      image_tag "flyfish-800w.jpg", class:"img-fluid"
+    end
+  end
+
+  def product_linked_image(product, href)
+    link_to href do
+      product_image_tag product
+    end
+  end
 end
