065a71e922691c75f5207f8de87dc390f06d4c45
Mischa Lusteck <mlusteck@googlemail.com>
2018-07-30 19:02:01 +0200

    5.2 Add search field to the navbar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  # to prevent case sensitive search (in postgres) convert all to lowercase
+  def self.search(search_term)
+    Product.where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
+  end
 end
