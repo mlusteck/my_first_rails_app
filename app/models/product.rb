68af8f555ff9b74871547f8ba88385c087cae744
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-03 16:52:38 +0200

    5.7 Create comments model and scopes for rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   # to prevent case sensitive search (in postgres) convert all to lowercase
   def self.search(search_term)
     Product.where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 end
