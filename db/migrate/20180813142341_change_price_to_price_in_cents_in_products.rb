6d7ea738394229cfb5d1a5edfdb99a616e89136a
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-13 19:48:29 +0200

    6.5 Add check for SQLite3 in the price conversion migration

--- db/migrate/20180813142341_change_price_to_price_in_cents_in_products.rb
+++ db/migrate/20180813142341_change_price_to_price_in_cents_in_products.rb
@@ -1,5 +1,21 @@
 class ChangePriceToPriceInCentsInProducts < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
+  def using_sqlite3?
+    ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
+  end
+
+  def sqlite3_turn_off_foreign_keys
+    if using_sqlite3?
+      execute("PRAGMA foreign_keys = OFF")
+    end
+  end
+
+  def sqlite3_turn_on_foreign_keys
+    if using_sqlite3?
+      execute("PRAGMA foreign_keys = ON")
+    end
+  end
+
   def up
     say_with_time "Converting product prices to cents(integer)..." do
       Product.all.each do |p|
@@ -12,31 +28,24 @@ class ChangePriceToPriceInCentsInProducts < ActiveRecord::Migration[5.2]
       end
     end
 
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
-      execute("PRAGMA foreign_keys = OFF")
-    end
+    sqlite3_turn_off_foreign_keys
     ActiveRecord::Base.transaction do
-      say "Test: " + execute("PRAGMA foreign_keys").to_s
+      if using_sqlite3?
+        say "Test: " + execute("PRAGMA foreign_keys").to_s
+      end
       change_column :products, :price, :integer, using: 'price::integer'
       rename_column :products, :price, :price_in_cents
     end
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
-      execute("PRAGMA foreign_keys = ON")
-    end
+    sqlite3_turn_on_foreign_keys
   end
 
   def down
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
-      execute("PRAGMA foreign_keys = OFF")
-    end
+    sqlite3_turn_off_foreign_keys
     ActiveRecord::Base.transaction do
       rename_column :products, :price_in_cents, :price
       change_column :products, :price, :decimal, using: 'price::decimal'
     end
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
-      execute("PRAGMA foreign_keys = ON")
-    end
-
+    sqlite3_turn_on_foreign_keys
     Product.reset_column_information
 
     say_with_time "Converting product prices to euros(dcimal)..." do
