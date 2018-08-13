3a4a6075891d914ecb20b8bb780ed53b919db943
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-13 19:33:36 +0200

    6.5 Add check for SQLite3 in the price conversion migration

--- db/migrate/20180813142341_change_price_to_price_in_cents_in_products.rb
+++ db/migrate/20180813142341_change_price_to_price_in_cents_in_products.rb
@@ -12,22 +12,30 @@ class ChangePriceToPriceInCentsInProducts < ActiveRecord::Migration[5.2]
       end
     end
 
-    execute("PRAGMA foreign_keys = OFF")
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      execute("PRAGMA foreign_keys = OFF")
+    end
     ActiveRecord::Base.transaction do
       say "Test: " + execute("PRAGMA foreign_keys").to_s
       change_column :products, :price, :integer, using: 'price::integer'
       rename_column :products, :price, :price_in_cents
     end
-    execute("PRAGMA foreign_keys = ON")
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      execute("PRAGMA foreign_keys = ON")
+    end
   end
 
   def down
-    execute("PRAGMA foreign_keys = OFF")
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      execute("PRAGMA foreign_keys = OFF")
+    end
     ActiveRecord::Base.transaction do
       rename_column :products, :price_in_cents, :price
       change_column :products, :price, :decimal, using: 'price::decimal'
     end
-    execute("PRAGMA foreign_keys = ON")
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      execute("PRAGMA foreign_keys = ON")
+    end
 
     Product.reset_column_information
 
