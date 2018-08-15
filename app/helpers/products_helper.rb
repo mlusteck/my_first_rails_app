3ccc9bf004014fb364480b217004c6af99c2f19a
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-15 21:52:24 +0200

    6.9 Add elevateZoom to product#show

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -8,6 +8,14 @@ module ProductsHelper
     end
   end
 
+  def product_zoom_image_tag(product)
+    if asset_exist? product.image_url
+      image_tag product.image_url, class: "img-fluid img-zoom", alt: product.name, data_zoom_image: product.image_url
+    else
+      image_tag "flyfish-800w.jpg", class: "img-fluid img-zoom", alt: product.name, data_zoom_image: "flyfish-800w.jpg"
+    end
+  end
+
   def product_image_url(product)
     if asset_exist? product.image_url
       image_url product.image_url
