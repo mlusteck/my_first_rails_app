95d7363759e880ce728cbe8560eba282e9b1ccb2
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-20 15:37:18 +0200

    6.11 Include UsersHelper in ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  helper UsersHelper
+  include UsersHelper
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
 
