b81138442dd8e042c02a4e18822e5351094ab4a5
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-29 14:54:03 +0200

    6.14 Debugging comments on heroku

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,7 +1,6 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
-    console.log("ProductChannel - Connected");
     this.isConnected = true;
   },
 
@@ -11,27 +10,20 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    //$(".alert.alert-info").show();
     $(".alert.alert-info").slideDown();
     setTimeout( function() {
       $(".alert.alert-info").slideUp();
-      //$(".alert.alert-info").hide();
     }, 3000);
     $.get(data.comment_path);
-    //$("#comments .product-reviews").prepend(data.comment);
-    //changeAverageRating(data.average_rating);
-    //refreshRatyRating();
 
   },
   listenToComments: function() {
     if( this.isConnected ) {
-      console.log("*** Start listenToComments " + this.isConnected + " --- " + $("[data-product-id]").data("product-id"))
       return this.perform('listen',
         { product_id: $("[data-product-id]").data("product-id") }
       );
     }
     else {
-      console.log("listenToComments: Waiting for connection...");
       setTimeout(function() { App.product.listenToComments()},100)
     }
   }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,7 @@ class Comment < ApplicationRecord
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
-  #after_create_commit { CommentUpdateJob.perform_later(self) }
+  after_create_commit { CommentUpdateJob.perform_later(self) }
 
   def html_id
     "comment_" + self.id.to_s
