291a3b96ef42773c5d6e826d74fbb29c3cc85e62
Mischa Lusteck <mlusteck@googlemail.com>
2018-08-29 13:35:16 +0200

    6.14 Debugging comments on heroku

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -23,6 +23,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   },
 
   listenToComments: function() {
+    console.log("*** Start listenToComments " + $("[data-product-id]").data("product-id"))
     return this.perform('listen',
       { product_id: $("[data-product-id]").data("product-id") }
     );
